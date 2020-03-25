4bb99f333ac0eb33176c80d6af2cb0e727418935
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-25 17:53:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+array = ["fiddleheads","okra","kohlrabi"]
 
+
+def oxford_comma(array)
+  if array.size == 1 
+    array.join
+  elsif array.size == 2 
+    array.join(" and ")
+  #this passes for 3 elements arrays and more
+  else
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  end 
 end
+
+
+
+puts oxford_comma(array)
+
+
+
